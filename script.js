e3fdea984b2de2b25d3fac9ca2a48033011f18e4
Muhammad Muaz <muhammadmuaz56@gmail.com>
2024-06-01 18:13:11 +0500

    Project objectives achieved
    Added all the required portions in .html, .css and
    .js files. The canvas is working as expected with
    no issues as of yet.

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+// Making the grid functional
 const container = document.querySelector(".container");
- 
-for (let i = 0; i < 16; i++) {
-    let top_div = document.createElement("div");
-    container.appendChild(top_div);
 
-    for (let j = 0; j < 16; j++) { 
-        let lower_div = document.createElement("div");
-        lower_div.setAttribute("style", "padding: 10px;");
-        
-        lower_div.addEventListener("mouseover", () => {
-            lower_div.style.backgroundColor = "black";
-        })
-        // lower_div.textContent = "wow";
-        top_div.appendChild(lower_div);
+function createGrid(size) {
+    let outer = document.querySelector(".outer");
+    let statement = document.createElement("p");
+    statement.textContent = "Try swiping the cursor below!"
+    outer.insertBefore(statement, container);
+
+    for (let i = 0; i < size; i++) {
+        let top_div = document.createElement("div");
+        container.appendChild(top_div);
+
+        for (let j = 0; j < size; j++) { 
+            let lower_div = document.createElement("div");
+            boxSize = 160 / size;
+            lower_div.setAttribute("style", `padding: ${boxSize}px;`);
+            
+            lower_div.addEventListener("mouseover", () => {
+                lower_div.style.backgroundColor = "black";
+            })
+            top_div.appendChild(lower_div);
+        }
     }
 }
+
+// Making the button functional
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let getSize = prompt("Enter a number of squares for each side: ");
+    gridSize = Number(getSize);
+    if (gridSize > 3 && gridSize <= 100) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        createGrid(gridSize);
+        
+        document.createElement("p")
+    }
+    else {
+        alert("Please enter a size between 3 and 100.");
+    }
+})
